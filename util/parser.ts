b85df468150a3e7b6a777c1d156c9300ef141d67
Michael Jensen <emjay1988@gmail.com>
2021-12-01 22:39:02 -0800

    Add some colors

--- util/parser.ts
+++ util/parser.ts
@@ -1,7 +1,10 @@
 import {readFileSync} from 'fs'
+import {yellow} from 'colors/safe'
 
 function parseFile(name: string): string[] {
-  return readFileSync(name, {encoding: 'utf8'}).split('\n').filter(x => x)
+  const result = readFileSync(name, {encoding: 'utf8'}).split('\n').filter(x => x)
+  console.log(yellow(`Loaded ${result.length} lines from ${name}`))
+  return result
 }
 
 export default parseFile
